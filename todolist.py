f1f5ff6afba870025a6723d576b56991ee9c426e
Terry Miller <millerterryt@gmail.com>
2022-10-18 17:58:37 -0400

    Changes to python file.

--- todolist.py
+++ todolist.py
@@ -1,3 +1,18 @@
+### Beginning of the script
+
+### Create a user menu
+### Options: Choose existing user, Create new user
+
+print("What would you like to do?")
+existing_user = "1. Choose existing user"
+new_user = "2. Create new user"
+print(existing_user)
+#insert line break here
+print(new_user)
+
+print("Welcome, (user_name)")
+
+### After user selection, choose an option.
 
 Choose = input("Welcome, please choose an option. \n ")
 
